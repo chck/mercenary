12b463c3776d7e4d0153f95642bc43575016dc9c
chck <candysynd@gmail.com>
2016-12-29 02:45:08 +0900

    [add] paging spider to spiders

--- mercenary/spiders/furusato_tax.py
+++ mercenary/spiders/furusato_tax.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from urllib import parse
+from bs4 import BeautifulSoup
 
 
-class FurusatoTaxSpider(scrapy.Spider):
-    name = "furusato-tax"
+
+class FurusatoTaxPagingSpider(scrapy.Spider):
+    name = "furusato-tax-paging"
     allowed_domains = ["furusato-tax.jp"]
-    start_urls = ['http://furusato-tax.jp/']
+    base_url = "http://{}".format(allowed_domains[0])
+
+    start_urls = [
+        "{}/search.html?target=1&q={}".format(base_url, parse.quote("いくら")),
+    ]
 
     def parse(self, response):
-        pass
+        soup = BeautifulSoup(response.body, "lxml")
+
+        next_page = soup.find('li', class_='active').findNextSibling('li')
+        # 次のページが見つからない場合は終了
+        if not next_page:
+            yield
+
+        path = next_page.a['href']
+        url = "{}{}".format(self.base_url, path)
+        print(url)
+
+        # scrapy.Requestを返すと次のCrawl対象としてEnqueueされる
+        next_crawl_page = scrapy.Request(url)
+        yield next_crawl_page
