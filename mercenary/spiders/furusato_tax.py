205457016581d23f7b932c546cf15729af863cc1
chck <candysynd@gmail.com>
2016-12-30 02:16:57 +0900

    [merge] paging spider and product one

--- mercenary/spiders/furusato_tax.py
+++ mercenary/spiders/furusato_tax.py
@@ -22,64 +22,47 @@ class FurusatoTaxPagingSpider(scrapy.Spider):
     def parse(self, response):
         soup = BeautifulSoup(response.body, "lxml")
 
-        next_page = soup.find('li', class_='active').findNextSibling('li')
+        products = soup.findAll("div", class_="thumbnail-pickUp-pt2 bg-normal col-xs-12")
+        products = [product.find("a", target="_blank") for product in products if self.query in product.text]
+        for product in products:
+            product_url = "{}{}".format(self.base_url, product.attrs['href'])
+            spider = FurusatoTaxProductSpider(product_url)
+            yield scrapy.Request(product_url, callback=spider.parse)
+
+        next_page = soup.find("li", class_="active").findNextSibling("li")
         # 次のページが見つからない場合は終了
         if not next_page:
             yield
-
-        path = next_page.a['href']
-        url = "{}{}".format(self.base_url, path)
-        print(url)
-
-        # scrapy.Requestを返すと次のCrawl対象としてEnqueueされる
-        next_crawl_page = scrapy.Request(url)
-        yield next_crawl_page
+        else:
+            # scrapy.Requestを返すと次のCrawl対象としてEnqueueされる
+            next_url = "{}{}".format(self.base_url, next_page.a['href'])
+            yield scrapy.Request(next_url)
 
 
-class FurusatoTaxProductsSpider(scrapy.Spider):
-    name = "furusato-tax-products"
+class FurusatoTaxProductSpider(scrapy.Spider):
+    name = "furusato-tax-product"
     allowed_domains = ["furusato-tax.jp"]
     base_url = "http://{}".format(allowed_domains[0])
 
-    def __init__(self, page_path, *args, **kwargs):
-        super(FurusatoTaxProductsSpider, self).__init__(*args, **kwargs)
-        self.page_path = page_path
+    def __init__(self, url, *args, **kwargs):
+        super(FurusatoTaxProductSpider, self).__init__(*args, **kwargs)
         self.start_urls = [
-            "{}/search.html{}".format(self.base_url, page_path),
+            url,
         ]
 
     def parse(self, response):
         soup = BeautifulSoup(response.body, "lxml")
 
-        products = soup.findAll("div", class_="thumbnail-pickUp-pt2 bg-normal col-xs-12")
-        products = [product.find("a", target="_blank") for product in products]
-        for product in products:
-            # if "セット" in product.text:
-            # print(product.text)
-            # print(product.attrs['href'])
-            url = "{}{}".format(self.base_url, product.attrs['href'])
-            yield scrapy.Request(url)
-
-
-class FurusatoTaxCpSpider(scrapy.Spider):
-    name = "furusato-tax-cp"
-    allowed_domains = ["furusato-tax.jp"]
-    base_url = "http://{}".format(allowed_domains[0])
-
-    def __init__(self, product_id, *args, **kwargs):
-        super(FurusatoTaxCpSpider, self).__init__(*args, **kwargs)
-        self.product_id = product_id
-        self.start_urls = [
-            "{}/japan/prefecture/item_detail/{}".format(self.base_url, product_id),
-        ]
-
-    def parse(self, response):
-        soup = BeautifulSoup(response.body, "lxml")
+        title = soup.find("h1", class_="itemDitailh1_sp_fontSize mT05").text
+        locality = soup.find("div", class_="titlePrefectures").text
+        price = soup.find("div", class_="clearfix text-right mT10") \
+            .find("span", class_="fs_32 text-red").text.replace(",", "")
 
         quantity_candidate = soup.find("div", class_="floatR non_floatRsp item_text").find("dd")
         if quantity_candidate:
             qc = mojimoji.zen_to_han(quantity_candidate.text, kana=False)
-            quantities = re.findall(r"\d+[mk]?[gm本個]", qc)
+            # TODO: 袋、尾、切、入、枚、箱、玉、肩、パック(PC)、\d+L(3L1kg, 3L4肩)、各\d+g、x\d+、小数点(1.7kg)、\d+人前、
+            quantities = re.findall(r"\d+[mk]?[lg㎏m本個]", qc)
             print("{}:::{}".format(qc, quantities))
             # TODO: 抽出できた量のうち個数と量の単位に分けて+とxをうまく演算する処理
 
